b498643ffb7f0313348d79ee83f723f967246428
BrunoBenedetto <bruno7.benedetto@gmail.com>
2026-08-07 15:18:35 -0400

    Ajuste para deploy no firebase

---
+++ js/config.properties.example.js
@@ -0,0 +1,8 @@
+// variavel usuário e login
+window.CELAB = window.CELAB || {};
+
+CELAB.CREDENCIAIS = {
+    admin: {usuario: 'admin', senha: 'root123'},
+    tecnico: {usuario: 'tecnico', senha: 'tecnico123'}
+};
+
